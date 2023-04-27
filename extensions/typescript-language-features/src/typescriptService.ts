9f25ce24f01ac121fa4d17d2ddfcbf24bbec15fa
Sheetal Nandi <shkamat@microsoft.com>
2023-04-26 22:03:24 -0700

    Changes for watchFactory

--- extensions/typescript-language-features/src/typescriptService.ts
+++ extensions/typescript-language-features/src/typescriptService.ts
@@ -85,6 +85,7 @@ interface NoResponseTsServerRequests {
 	'compilerOptionsForInferredProjects': [Proto.SetCompilerOptionsForInferredProjectsArgs, null];
 	'reloadProjects': [null, null];
 	'configurePlugin': [Proto.ConfigurePluginRequest, Proto.ConfigurePluginResponse];
+	'onWatchChange': [Proto.Request, null];
 }
 
 interface AsyncTsServerRequests {
